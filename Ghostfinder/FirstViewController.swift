0939e37b849a448ab1565ff2e42a341de531c1ab
Bruno Omella Mainieri <bruno.omella@hotmail.com>
2019-06-14 15:39:39 -0300

    Initial Commit

---
+++ Ghostfinder/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Ghostfinder
+//
+//  Created by Bruno Omella Mainieri on 14/06/19.
+//  Copyright © 2019 Bruno Omella Mainieri. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
